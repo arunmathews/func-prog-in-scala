35bfb7a1fff1e1d4c596597b53a5ee494bb39755
Arun Mathews <arun.a.mathews@gmail.com>
2014-04-02 03:48:35 -0700

    Trying to design a minimal algebra for parser combinator

--- src/main/scala/chap9/ChapterExercisesSource.scala
+++ src/main/scala/chap9/ChapterExercisesSource.scala
@@ -8,7 +8,7 @@ object ChapterExercisesSource {
   trait Parser[+A]
   trait ParseError
 
-  trait Parsers[ParseError, Parser[+_]] {
+  trait Parsers[ParseError, Parser[+_]] { self =>
     def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
     //should satisfy run(char(c))(c.toString) == Right(c)
@@ -25,8 +25,25 @@ object ChapterExercisesSource {
 
     def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
 
+    def map[A, B](a : Parser[A])(f: A => B): Parser[B]
+
+    def map2[A, B, C](a: Parser[A], b: Parser[B])(f: (A, B) => C): Parser[C]
+
+    def flatMap[A, B](a: Parser[A])(f: A => Parser[B]): Parser[B]
+
+    def many[A](p: Parser[A]): Parser[List[A]]
+
+    def zeroOrMore(c: Char): Parser[Int] = map(many(char(c)))(l => l.size)
+
+    def oneOrMore(c: Char): Parser[Int] = map(map2(char(c), many(char(c)))(_ :: _))(l => l.size)
+
+    def zeroAOneB(c1: Char, c2: Char): Parser[(Int, Int)] = map2(zeroOrMore(c1), oneOrMore(c2))((_, _))
+
+    def zeroCount(c: Char): Parser[Int] =
+
     case class ParserOps[A](p: Parser[A]) {
-      def |[B >:A](p2: Parser[B]): Parser[B] = or(p, p2)
+      def |[B >:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+      def or[B >:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     }
   }
 }
