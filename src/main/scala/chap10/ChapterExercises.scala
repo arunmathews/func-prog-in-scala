0384cdea024490fa97c93473d0ad092ccb443a47
Arun Mathews <arun.a.mathews@gmail.com>
2014-04-14 10:25:47 -0700

    Onto chap10. Monoids!!

---
+++ src/main/scala/chap10/ChapterExercises.scala
@@ -0,0 +1,51 @@
+package scala.chap10
+
+/**
+ * Code from chap10 - both source and exercises
+ */
+object ChapterExercises {
+  trait Monoid[A] {
+    def op(a1: A, a2: A): A
+
+    def zero: A
+  }
+
+  val stringMonoid = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+
+    override def zero: String = ""
+  }
+
+  def listMonoid[A] = new Monoid[List[A]] {
+    override def zero: List[A] = Nil
+
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+  }
+
+  //Ex 1
+  val intAddition = new Monoid[Int] {
+    override def zero: Int = 0
+
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+  }
+
+  val intMultiplication = new Monoid[Int] {
+    override def zero: Int = 1
+
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+  }
+
+  val booleanOr = new Monoid[Boolean] {
+    override def zero: Boolean = false
+
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+  }
+
+  val booleanAnd = new Monoid[Boolean] {
+    override def zero: Boolean = true
+
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+  }
+
+  
+}
---
+++ src/test/scala/chap10/ChapterExercisesTest.sc
@@ -0,0 +1,11 @@
+import scala.chap10.ChapterExercises._
+import scala.chap8.ChapterSamples.Prop
+
+listMonoid.zero
+
+listMonoid.op(listMonoid.zero, List(1,3,3))
+
+
+
+
+
