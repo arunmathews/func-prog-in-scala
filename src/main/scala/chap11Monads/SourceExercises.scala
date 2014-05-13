5227a5a9bc05a7922368641dfa9dbd55a65f30bb
Arun Mathews <arun.a.mathews@gmail.com>
2014-05-13 08:17:46 -0700

    Ex 1 of chapter 12 applicative done

--- src/main/scala/chap11Monads/SourceExercises.scala
+++ src/main/scala/chap11Monads/SourceExercises.scala
@@ -22,9 +22,11 @@ object SourceExercises {
   }
 
   trait Monad[F[_]] extends Functor[F] {
+    //primitives
     def unit[A](a: => A): F[A]
     def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
+    //derived
     override def map[A, B](ma: F[A])(f: A => B): F[B] =
       flatMap(ma)(a => unit(f(a)))
 
--- src/main/scala/chap12Applicative/SourceExercises.scala
+++ src/main/scala/chap12Applicative/SourceExercises.scala
@@ -7,6 +7,21 @@ import scala.chap11Monads.SourceExercises.Functor
  */
 object SourceExercises {
   trait Applicative[F[_]] extends Functor[F] {
+    //primitive combinators
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
 
+    def unit[A](a: => A): F[A]
+
+    //derived combinators
+    override def map[A, B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
+
+    def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]] = la.foldRight(unit(List[B]()))((a, lfb) => map2(f(a), lfb)(_ :: _))
+
+    //Ex 1
+    def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(fa => fa)
+
+    def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
+
+    def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa, fb)((a, b) => (a, b))
   }
 }
