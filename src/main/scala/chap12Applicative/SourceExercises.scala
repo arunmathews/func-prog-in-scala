0e33137a44a62c3c95759541a6098a9a406077da
Arun Mathews <arun.a.mathews@gmail.com>
2014-05-13 10:44:29 -0700

    Ex 2 of Chapter 12 done

--- src/main/scala/chap12Applicative/SourceExercises.scala
+++ src/main/scala/chap12Applicative/SourceExercises.scala
@@ -1,7 +1,7 @@
 package scala.chap12Applicative
 
 import scala.chap11Monads.SourceExercises.Functor
-
+import scala.language.higherKinds
 /**
  * Source code and exercises from chapter 12
  */
@@ -22,6 +22,15 @@ object SourceExercises {
 
     def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
 
-    def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa, fb)((a, b) => (a, b))
+    def product[A, B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa, fb)((a, b) => (a, b))
+
+    //Ex 2
+    def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)((aToB, a) => aToB(a))
+
+    def mapApply[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+
+    def map2Apply[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] = apply(apply(unit(f.curried))(fa))(fb)
+
   }
+
 }
