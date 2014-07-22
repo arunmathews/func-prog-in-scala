578452da50a4c693db43734ef1268a9b2d4f6010
Arun Mathews <arun.a.mathews@gmail.com>
2014-07-22 08:22:05 -0700

    Ex 4 of Monoids revision

--- src/main/scala/chap10MonoidsRev/SourcePlusExercises.scala
+++ src/main/scala/chap10MonoidsRev/SourcePlusExercises.scala
@@ -1,5 +1,6 @@
 package scala.chap10MonoidsRev
 
+import scala.chap8.ChapterSamples.{Gen, Prop}
 /**
  * Source code plus exercises from chapter 10 - Monoids. Monoid - category with one object
  */
@@ -8,6 +9,12 @@ object SourcePlusExercises {
     def op(a1: A, a2: A): A
 
     def zero: A
+  }
+
+  def dual[A](m: Monoid[A]): Monoid[A] = new Monoid[A] {
+    override def op(a1: A, a2: A): A = m.op(a2, a1)
+
+    override def zero: A = m.zero
   }
 
   val stringMonoid = new Monoid[String] {
@@ -24,8 +31,51 @@ object SourcePlusExercises {
 
   //Ex 1
   val intAddition: Monoid[Int] = new Monoid[Int] {
-    override def op(a1: Int, a2: Int): Int = ???
+    override def op(a1: Int, a2: Int): Int = a1 + a2
 
-    override def zero: Int = ???
+    override def zero: Int = 0
   }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+
+    override def zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero: Boolean = true
+  }
+
+  //Ex 2
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+
+    override def zero: Option[A] = None
+  }
+
+  def firstOptionMonoid[A]: Monoid[Option[A]] = optionMonoid
+  def lastOptionMonoid[A]: Monoid[Option[A]] = dual(firstOptionMonoid)
+
+  //Ex 3
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: (A) => A, a2: (A) => A): (A) => A = a1 andThen a2
+
+    override def zero: (A) => A = (a:A) => a
+  }
+
+  //Ex 4
+  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = Prop.forAll(for {
+    x <- gen
+    y <- gen
+    z <- gen
+  } yield (x, y, z))(p => m.op(p._1, m.op(p._2, p._3)) == m.op(m.op(p._1, p._2), p._3)) &&
+  Prop.forAll(gen)((a: A) => m.op(a, m.zero) == a && m.op(a, m.zero) == a)
 }
