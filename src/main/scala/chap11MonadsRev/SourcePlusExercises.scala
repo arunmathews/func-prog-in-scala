e7ef9e1cd00c92d531b955db7910b73cda6bc7e5
Arun Mathews <arun.a.mathews@gmail.com>
2014-08-18 08:42:01 -0700

    Ex 7 of monads rev done

--- src/main/scala/chap11MonadsRev/SourcePlusExercises.scala
+++ src/main/scala/chap11MonadsRev/SourcePlusExercises.scala
@@ -79,6 +79,9 @@ object SourcePlusExercises {
        f(v).flatMap(g)  == (a => f(a).flatMap(g))(v)
        f(v).flatMap(g) == f(v).flatMap(g) QED
     */
+
+    //Ex 7
+    def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] = (a: A) => flatMap(f(a))(g)
   }
 
   object Monad {
